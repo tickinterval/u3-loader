983ceffe465b9e4732d83017a2433bfdcc0a89d1
tickinterval <w1shhvh@gmail.com>
2025-12-30 17:58:08 +0000

    Bump loader version to 1.3.0

--- src/app_state.cpp
+++ src/app_state.cpp
@@ -21,7 +21,7 @@ const COLORREF kAccentAlt = RGB(89, 129, 235);
 const COLORREF kMutedColor = RGB(144, 154, 170);
 const COLORREF kMaskColor = RGB(255, 0, 255);
 
-const char kLoaderVersion[] = "1.2.2";
+const char kLoaderVersion[] = "1.3.0";
 const wchar_t kDefaultServerUrl[] = L"tcps://45.8.145.200:4000";
 const wchar_t kDefaultExpectedThumbprint[] = L"0219BB3E2098384C323E494FA285FC594993951A6822852764B163CBA7F273D2"; // Set to TLS cert thumbprint (hex) for pinning.
 const wchar_t kDefaultUserAgent[] = L"u3ware/1.0";
